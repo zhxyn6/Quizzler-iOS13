dfab13d12d112e550a74fabb8ac56cd214baa558
Xinran <zhxyn6@gmail.com>
2023-01-10 16:06:43 -0500

    build a quiz app
    Loop through the questions in the list of questions. Come back to the first one after traversing the entire list

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["1+1 = 2", "True"],
+        ["3-1=1", "False"],
+        ["4-2=3", "False"],
+        ["5+3 = 8", "True"]
+    ]
+    
+    var quesNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // initialize the question once onpe the app
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[quesNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Correct!")
+        } else {
+            print("Wrong...")
+        }
+        
+        // each time trigger the button, update the question
+//        quesNumber = Int.random(in: 0...3)
+        if quesNumber + 1 < quiz.count {
+            quesNumber += 1
+        } else {
+            quesNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[quesNumber][0]
+    }
 }
 
